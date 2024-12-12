5241ffb1661a295fb882e535598fbc49b0d6f1ad
Denis4005 <den.klimov.01@yandex.ru>
2024-12-12 19:37:10 +0300

    second commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     {
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(
-                30
+                27
             )->by($request->user()?->id ?: $request->ip())->response(function (Request $request, array $headers) {
                 return response()->json([
                     'message' => "Too Many Requests. Please try again later."
